bcffad3302000ef516aa835d5d3c981fdea6b3b2
Luca Peric <lucaperic.lp@gmail.com>
2021-08-01 19:18:39 +0100

    Updating with sessions & tasks

---
+++ sessions/session_09.py
@@ -0,0 +1,6 @@
+def my_function(first_arg: int) -> int:
+    # recursion
+    return my_function(first_arg)
+
+my_var = 10
+my_function(my_var)
---
+++ sessions/session_10.py
@@ -0,0 +1,16 @@
+# recursion
+def my_func(a: int) -> None:
+    print(a)
+    if a < 100:
+        return my_func(a+1)
+    return
+
+my_func(5)
+
+####
+
+# Mapping & Dict1 & Dict2 & Dict3
+
+####
+
+# Node traversal
